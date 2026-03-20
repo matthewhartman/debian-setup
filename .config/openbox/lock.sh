b24d9b2f9e2e5afd20e3c8231b1f180a2ef7459a
Matthew <matthewhartmanau@gmail.com>
2026-03-21 10:18:14 +1100

    remove clock and date from lock screen

--- .config/openbox/lock.sh
+++ .config/openbox/lock.sh
@@ -6,18 +6,7 @@ IMG="/tmp/lockscreen.png"
 scrot "$IMG"
 
 # Blur + dim
-convert "$IMG" \
-  -blur 0x8 \
-  -fill black -colorize 20% \
-  -gravity center \
-  -fill white \
-  -font DejaVu-Sans \
-  -pointsize 72 \
-  -annotate +0-20 "$(date '+%H:%M')" \
-  -pointsize 24 \
-  -fill "#dddddd" \
-  -annotate +0+40 "$(date '+%A, %d %B')" \
-  "$IMG"
+convert "$IMG" -blur 0x8 -fill black -colorize 20% "$IMG"
 
 # Lock using blurred screenshot
 i3lock -i "$IMG" --nofork --no-unlock-indicator
